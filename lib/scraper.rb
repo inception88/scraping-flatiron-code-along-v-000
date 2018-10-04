767d21c3aed352f608e4428197e6c602c6360060
Benjamin Jones <bljones36@gmail.com>
2018-10-04 16:11:47 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -29,7 +29,7 @@ class Scraper
       Course.all.each do |course|
       if course.title
         puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
+        puts "Schedule: #{course.schedule}"
         puts "  Description: #{course.description}"
       end
       end
